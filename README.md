e543257cf5623e9d7ba1b01ae76b7185ed3f7484
kaka <63922370+ruyiq@users.noreply.github.com>
2020-06-13 19:25:49 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Pixel Art Maker Project
 
-# Still on progress.
+## About THIS PROJECT
+
+This is a simple editor intended to be used for pixel-art creation and will get updated over time. This editor is built in JavaScript, HTML and CSS.
+
+## Contributing?
+
+ALWAYS WELCOME!
--- designs.js
+++ designs.js
@@ -1,30 +1,45 @@
-const canvas = document.getElementById('pixelCanvas')
-const color = document.getElementById('colorPicker')
-const tbody = document.createElement('tbody');
+const table = document.getElementById("pixelCanvas");
+const cells = document.getElementsByClassName('cell');
+const sizePicker = document.getElementById('sizePicker');
 
-// Store the value of columns
-
-// Access forms
-const size = document.getElementById('sizePicker');
-
-size.addEventListener('submit', function(e){
-  e.preventDefault();  // Prevents the submit button from refreshing the page by default
-	tbody.innerHTML='';
-  const column = document.getElementById('inputWidth').value;
-
-// Store the value of rows
-const row = document.getElementById('inputHeight').value;
-  console.log(row);
-  for (r = 0; r < row; r++) {
-    const tr = document.createElement('tr');
-    tbody.appendChild(tr);
-    for (c = 0; c < column; c++) {
-      const td = document.createElement('td');
-      tr.appendChild(td);
-    }
-    canvas.appendChild(tbody);
-  }
-
-canvas.appendChild(tbody);
-
+// config sizePicker
+sizePicker.addEventListener('submit',function(){
+  event.preventDefault();
+  const height = document.getElementById('inputHeight').value;
+  const width = document.getElementById('inputWidth').value;
+  makeGrid(height, width);
 });
+
+//reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/tr
+function makeGrid(numrows, numcols) {
+    let table_body = '';
+    // add row to table body
+    for (let r = 0; r < numrows; r++) {
+        table_body += '<tr class="row-' + r + '">';
+        // add columns to table body
+        for (let c = 0; c < numcols; c++) {
+            table_body += '<td class="cell" id="row-' + r + '_cell-' + c + '"></td>';
+        }
+        table_body += '</tr>';
+    }
+    table.innerHTML = table_body;
+
+    // update color once a click is made
+    update_color();
+}
+
+// add click events to all cells
+function update_color() {
+  //loop over all cells and try to find the one being clicked.
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", function (e) {
+            let clickedCell = e.target;
+            const color = document.getElementById("colorPicker").value;
+            clickedCell.style.backgroundColor = color;
+        });
+    }
+}
+
+
+// Build a default 10x10 grid.
+makeGrid(10, 10);
